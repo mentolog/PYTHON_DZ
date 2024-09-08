2b6b9b1c21dc1106e33971eec806fe392c30c979
Maxim Bocharov <rr21911@yandex.ru>
2024-09-08 09:34:07 +0300

    COMMIT #4 - 08.09.2024

--- lesson2/02_lesson/lesson_2_task_3.py
+++ lesson2/02_lesson/lesson_2_task_3.py
@@ -1,30 +1,25 @@
 import math
 
 def square(side):
-    return side * side
+    return math.ceil(side) * math.ceil(side)
 
 attempts = 0
 max_attempts = 3
 
 while attempts < max_attempts:
     try:
-        side = input("Введите сторону квадрата от 0 до 100 (в сантиметрах): ")
+        side = float(input("Введите сторону квадрата от 1 до 100 (в сантиметрах): "))
         
-        # Проверка на целое число
-        if not side.isdigit():
-            raise ValueError("Вы можете ввести только целое положительное число - от 0 до 100!")
-        
-        side = int(side)
-
-        if 0 <= side <= 100:
+        # Проверка на допустимый диапазон
+        if 1 <= side <= 100:
             area = square(side)
             print(f"Площадь квадрата равна {area} сантиметров.")
             break
         else:
-            print("Введенное число должно быть от 0 до 100.")
-            
-    except ValueError as e:
-        print(e)
+            print("Введенное число должно быть от 1 до 100.")
+
+    except ValueError:
+        print("Вы можете ввести только число в диапазоне от 1 до 100 с точностью до четырех знаков после точки.")
     
     attempts += 1
 
